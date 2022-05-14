16af54cf4084dcb3b431ffdc7680be822ff18a80
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2022-05-14 20:22:29 -0300

    Adição do Banco de Dados

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -3,10 +3,15 @@ from flask import Blueprint, request
 from flask_restx import Resource, Api, fields
 
 admin_bp = Blueprint('admin',__name__)
-api = Api(admin_bp)
 
-@api.route('/teste', methods=['GET'])
-class Show_Admin(Resource):
-    def get(self):
+@admin_bp.route('/')
+def index():
+    return '<h1>Painel Admnistrativo!</h1>'
+
+#api = Api(admin_bp)
+
+# @api.route('/teste', methods=['GET'])
+# class Show_Admin(Resource):
+#     def get(self):
         
-        return {'Status':'OK', 'message':'I am Admin'}, 200
+#         return {'Status':'OK', 'message':'I am Admin'}, 200
