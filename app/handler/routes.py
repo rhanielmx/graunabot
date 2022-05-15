a2cd4251463534732e8b72b4c51da8fd181fcd28
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2022-05-14 21:42:16 -0300

    Criando interface com banco de dados

--- app/handler/routes.py
+++ app/handler/routes.py
@@ -1,7 +1,9 @@
 
-from app import app
+from app import app, db
 from flask import Blueprint, request
 from flask_restx import Resource, Api, fields
+
+from app.models import News
 
 handler_bp = Blueprint('handler',__name__)
 api = Api(handler_bp)
@@ -14,13 +16,26 @@ api = Api(handler_bp)
 
 message_model = api.model('Message',{
     'message': fields.String,
-    'num-pedido': fields.String
+    'requestNumber': fields.String
 })
 
-@api.route('/teste', methods=['POST'])
+@api.route('/new', methods=['POST'])
 class Create_Solicitacao(Resource):
     @api.expect(message_model)
     def post(self):
         msg = request.json['message']
-        print(request.json)
+        data = News(id=12131,**request.json)
+        data.save()
         return {'Status':'OK', 'message':msg}, 200
+
+@api.route('/list', methods=['GET'])
+class ListNews(Resource):
+    def get(self):
+        data = News.query.all()
+        return {'Status':'OK', 'News':[d.json() for d in data]}, 200
+
+@api.route('/list/<int:id>', methods=['GET'])
+class ListNews(Resource):
+    def get(self, id):
+        data = News.query.filter_by(id=id).first()
+        return {'Status':'OK', 'News':data.json()}, 200
--- app/models.py
+++ app/models.py
@@ -1,36 +1,70 @@
 from app import db
+import enum
 
-class User(db.Model):
-    __tablename__ = 'users'
+# class User(db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     username = db.Column(db.String(50))
+#     password = db.Column(db.String(50))
+#     type = db.Column(db.String(50))
+
+#     @property
+#     def is_authenticated(self):
+#         return True
+
+#     @property
+#     def is_active(self):
+#         return True
+
+#     @property
+#     def is_anonymous(self):
+#         return False
+
+#     def get_id(self):
+#         return str(self.id)
+
+
+#     def __init__(self, name, username, password, type):
+#         self.name = name
+#         self.username = username
+#         self.password = password
+#         self.type = type
+
+#     __mapper_args__ = {
+#         'polymorphic_identity': 'users',
+#         'polymorphic_on': type
+#     }
+
+class NewsStatusEnum(enum.Enum):
+    pending='pending'
+    real='real'
+    fake='fake'
+
+class News(db.Model):
+    __tablename__ = 'news'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    username = db.Column(db.String(50))
-    password = db.Column(db.String(50))
-    type = db.Column(db.String(50))
+    message = db.Column(db.String(500))
+    requestNumber = db.Column(db.String)
+    status = db.Column(db.String)#db.Enum(default=NewsStatusEnum.pending, nullable=False))
 
-    @property
-    def is_authenticated(self):
-        return True
+    def __init__(self, id, message, requestNumber) -> None:
+        self.id = id
+        self.message = message
+        self.requestNumber = requestNumber
+        self.status = 'pending'
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
-    @property
-    def is_active(self):
-        return True
+    def json(self):
+        return {
+            'id': self.id,
+            'message': self.message,
+            'requestNumber': self.requestNumber,
+            'status': self.status
+        }
 
-    @property
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return str(self.id)
-
-
-    def __init__(self, name, username, password, type):
-        self.name = name
-        self.username = username
-        self.password = password
-        self.type = type
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'users',
-        'polymorphic_on': type
-    }
+    def __str__(self) -> str:
+        return self.status
