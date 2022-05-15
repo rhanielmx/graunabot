29f546852a73cea8c9342c52e79b9eadf31b9bde
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2022-05-14 21:45:50 -0300

    Criação e configuração do arquivo .env

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ load_dotenv(verbose=True)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+print(os.getenv('SQLALCHEMY_DATABASE_URI'))
+
 class Config(object):
     DEBUG=True
     SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(basedir, 'data.db')
