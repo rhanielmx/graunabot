42ac88e249bfad08df810e714c6451c54489138e
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2022-05-18 02:16:25 -0300

    Correção na engine de conexão com o BD

--- config.py
+++ config.py
@@ -17,5 +17,5 @@ print(POSTGRES_URL)
 class Config(object):
     DEBUG=True
     #SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(basedir, 'data.db')
-    SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
