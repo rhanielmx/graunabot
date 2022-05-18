22cf9229472877ffd06ad2a73b8cb9de2809d760
Rhaniel Xavier <rhanielmag.xavier@gmail.com>
2022-05-18 02:08:04 -0300

    Atualizando banco de dados para o postgres

--- config.py
+++ config.py
@@ -5,7 +5,17 @@ load_dotenv(verbose=True)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+ENGINE = os.getenv('ENGINE')
+USER = os.environ.get('USER')
+PASSWORD = os.getenv('PASSWORD')
+HOST = os.getenv('HOST')
+DBNAME = os.environ.get('DBNAME')
+
+POSTGRES_URL = f'{ENGINE}://{USER}:{PASSWORD}@{HOST}/{DBNAME}'
+print(POSTGRES_URL)
+
 class Config(object):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(basedir, 'data.db')
+    #SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(basedir, 'data.db')
+    SQLALCHEMY_DATABASE_URI='postgresql://teste:admin@localhost/teste'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
